77cde00c4cb1301a5ab3f463f8b30f3b3ea548f3
stembrino <stembrino@gmail.com>
2024-10-18 21:05:42 -0300

    init

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const tsParser = require("@typescript-eslint/parser");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const prettierPlugin = require("eslint-plugin-prettier");
+const prettierConfig = require("eslint-config-prettier");
+
+module.exports = [
+  {
+    files: ["**/*.{ts,js}"],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "prettier/prettier": "error",
+      ...prettierConfig.rules,
+    },
+  },
+];
